111b8c94afdc39111064296430d4b114e02b9bc2
Leo Generali <leogen17@gmail.com>
2017-02-08 00:13:58 -0500

    More fixes to the final async function

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,16 +52,7 @@ $(function() {
         });
     });
 
-
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function(){
-        
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
 
         beforeEach(function(done){
             loadFeed(0, done);  
@@ -83,12 +74,12 @@ $(function() {
 
         beforeEach(function(done){
             loadFeed(0, function(){
-                currentFeed = $('.feed').children();
+                currentFeed = $('.feed').html();
+                loadFeed(1, function(){
+                    newFeed = $('.feed').html();
+                    done();
+                });   
             });
-            loadFeed(1, function(){
-                newFeed = $('.feed').children();
-                done();
-            });   
         });
         
         it('has loaded a new feed', function(){
